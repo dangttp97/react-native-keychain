64f5abc5d652ad514a1b25a67f2cf82cc58203cd
Trần Thành Phương Đăng <dangttp97@gmail.com>
2022-08-30 20:02:46 +0700

    fix: reduce key waiting time

--- android/src/main/java/com/oblador/keychain/cipherStorage/CipherStorageKeystoreRsaEcb.java
+++ android/src/main/java/com/oblador/keychain/cipherStorage/CipherStorageKeystoreRsaEcb.java
@@ -221,12 +221,22 @@ public class CipherStorageKeystoreRsaEcb extends CipherStorageBase {
 
     final int purposes = KeyProperties.PURPOSE_DECRYPT | KeyProperties.PURPOSE_ENCRYPT;
 
+    if(Build.VERSION_CODES.SDK_INT == Build.VERSION_CODES.R){
+      return return new KeyGenParameterSpec.Builder(alias, purposes)
+      .setBlockModes(BLOCK_MODE_ECB)
+      .setEncryptionPaddings(PADDING_PKCS1)
+      .setRandomizedEncryptionRequired(true)
+      .setUserAuthenticationRequired(true)
+      .setUserAuthenticationParameters(1, KeyProperties.AUTH_BIOMETRIC_STRONG)
+      .setKeySize(ENCRYPTION_KEY_SIZE);
+    }
+
     return new KeyGenParameterSpec.Builder(alias, purposes)
       .setBlockModes(BLOCK_MODE_ECB)
       .setEncryptionPaddings(PADDING_PKCS1)
       .setRandomizedEncryptionRequired(true)
       .setUserAuthenticationRequired(true)
-      .setUserAuthenticationValidityDurationSeconds(5)
+      .setUserAuthenticationValidityDurationSeconds(1)
       .setKeySize(ENCRYPTION_KEY_SIZE);
   }
 
